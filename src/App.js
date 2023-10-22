c6efa1867f5631006744ddaceabb18a8bafa3333
Shivansh-Mittal <shivanshmittal1444@gmail.com>
2023-10-22 10:48:21 +0530

    text form edited

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import About from './components/About';
+// import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 
@@ -8,8 +8,8 @@ function App() {
     <>
     <Navbar title='MextUtils' aboutUs='About' />
     <div className="container my-3" >
-      {/* <TextForm heading='Enter the text to analyze below' /> */}
-      <About />
+      <TextForm heading='Enter the text to analyze below' />
+      {/* <About /> */}
     </div>
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -26,18 +26,30 @@ export default function TextForm(props) {
     setupMyText(event.target.value);
   }
 
+  const handleCopyText = ()=>{
+    let areaText = document.getElementById('textFormBox');
+    areaText.select();
+    navigator.clipboard.writeText(areaText.value);
+  }
+
+  const handleExtraSpaces = ()=>{
+    setupMyText(text.split(/[ ]+/).join(' '))
+  }
+
   const [text, setupMyText] = useState('');
   return (
     <>
     <div className='container my-3'>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <textarea className="form-control" onChange={handleOnChange} value={text} id="textForm" rows="8"></textarea>
+            <textarea className="form-control" onChange={handleOnChange} value={text} id="textFormBox" rows="8"></textarea>
         </div>
         <button className="btn btn-primary mx-1" onClick={handleUpperClick}>Convert to Uppercase</button>
         <button className="btn btn-secondary mx-1" onClick={handleLowerClick}>Convert to Lowercase</button>
         <button className="btn btn-danger mx-1" onClick={handleClearClick}>Clear Text</button>
         <button className="btn btn-success mx-1" onClick={handleReverseClick}>Reverse Text</button>
+        <button className="btn btn-primary mx-1" onClick={handleCopyText}>Copy Text</button>
+        <button className="btn btn-secondary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
     </div>
 
     <div className="container my-3">
