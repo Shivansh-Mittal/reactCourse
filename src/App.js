2dc601b762f47f94c97adf10e611b4e879888b56
Shivansh-Mittal <shivanshmittal1444@gmail.com>
2023-10-22 21:02:38 +0530

    added alerts

--- src/App.js
+++ src/App.js
@@ -1,14 +1,43 @@
 import './App.css';
+import React, {useState} from 'react'
 // import About from './components/About';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import Alert from './components/Alert';
 
 function App() {
+  const [mode, setmode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      msg: message,
+      type: type
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 3000);
+  }
+
+  const toggleMode = ()=>{
+    if(mode==='light'){
+      setmode('dark');
+      document.body.style.backgroundColor = '#042743';
+      showAlert('Dark mode has been enabled', 'success');
+    }
+    else{
+      setmode('light');
+      document.body.style.backgroundColor = 'white'
+      showAlert('Light mode has been enabled', 'success');
+    }
+  }
+
   return (
     <>
-    <Navbar title='MextUtils' aboutUs='About' />
+    <Navbar title='MextUtils' aboutUs='About' mode={mode} toggleMode={toggleMode} />
+    <Alert alert={alert} />
     <div className="container my-3" >
-      <TextForm heading='Enter the text to analyze below' />
+      <TextForm showAlert={showAlert} heading='Enter the text to analyze below' mode={mode} />
       {/* <About /> */}
     </div>
     </>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -5,20 +5,24 @@ export default function TextForm(props) {
     // console.log("Uppercase was clicked");
     let newText = text.toUpperCase();
     setupMyText(newText);
+    props.showAlert('Converted to uppercase', 'success');
   }
-
+  
   const handleLowerClick = ()=>{
     // console.log("Uppercase was clicked");
     let newText = text.toLowerCase();
     setupMyText(newText);
+    props.showAlert('Converted to lowercase', 'success');
   }
-
+  
   const handleClearClick = ()=>{
     setupMyText('');
+    props.showAlert('Text cleared', 'danger');
   }
 
   const handleReverseClick = ()=>{
     setupMyText(text.split(" ").reverse().join(" "));
+    props.showAlert('Text Reversed!!', 'success');
   }
 
   const handleOnChange = (event)=>{
@@ -30,6 +34,7 @@ export default function TextForm(props) {
     let areaText = document.getElementById('textFormBox');
     areaText.select();
     navigator.clipboard.writeText(areaText.value);
+    props.showAlert('Copied to clipboard!!', 'success');
   }
 
   const handleExtraSpaces = ()=>{
@@ -38,11 +43,11 @@ export default function TextForm(props) {
 
   const [text, setupMyText] = useState('');
   return (
-    <>
-    <div className='container my-3'>
+  <>
+    <div className='container my-3' style={{color : props.mode==='dark'?'white':'black'}}>
         <h1>{props.heading}</h1>
         <div className="mb-3">
-            <textarea className="form-control" onChange={handleOnChange} value={text} id="textFormBox" rows="8"></textarea>
+          <textarea className="form-control" style={{backgroundColor : props.mode==='dark'?'#042743':'white', color: props.mode==='dark'?'white':'black'}} onChange={handleOnChange} value={text} id="textFormBox" rows="8"></textarea>
         </div>
         <button className="btn btn-primary mx-1" onClick={handleUpperClick}>Convert to Uppercase</button>
         <button className="btn btn-secondary mx-1" onClick={handleLowerClick}>Convert to Lowercase</button>
@@ -52,12 +57,12 @@ export default function TextForm(props) {
         <button className="btn btn-secondary mx-1" onClick={handleExtraSpaces}>Remove Extra Spaces</button>
     </div>
 
-    <div className="container my-3">
+    <div className="container my-3" style={{color : props.mode==='dark'?'white':'black'}}>
         <h2>Your Text Summary</h2>
         <p>Your text have {text.split(" ").length} words and {text.length} characters.</p>
         <h2>Preview</h2>
         <p>{text}</p>
     </div>
-    </>
+  </>
   )
 }
